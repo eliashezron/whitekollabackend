2e6bb13636d41fffa8f0d01e45c57d89e1a4cda9
eliashezron <eliashezron23@gmail.com>
2021-10-14 16:38:39 +0300

    ready to deploy
    getting reading to deploy to heroku

--- server.js
+++ server.js
@@ -55,8 +55,8 @@ app.use(cors())
 // app.use(express.static(__dirname + '/public'))
 app.use("/public", express.static(path.join(__dirname, "/public")))
 
-// app.get('/', (req, res)=>{
-//     res.send('this is the home page')
+app.get('/', (req, res)=>{
+    res.send('this is the home page')
 // })
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
@@ -69,12 +69,12 @@ app.use('/api/upload', uploadRoutes)
 
 // "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix client && npm run build --prefix client"
 // // deloying to the server
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '/client/build')))
+//if(process.env.NODE_ENV === 'production'){
+  //  app.use(express.static(path.join(__dirname, '/client/build')))
 
-    app.get('*', (req, res)=>
-     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
-}
+   // app.get('*', (req, res)=>
+    // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+//}
 // error middleware
 app.use(notFound)
 app.use(errorHandler)
